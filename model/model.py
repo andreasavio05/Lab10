5dfae2cbce2f1ab805059633df76625e6c78de55
andreasavio05 <andrea.savio777@gmail.com>
2025-12-01 19:23:02 +0100

    lab10

--- model/model.py
+++ model/model.py
@@ -12,7 +12,6 @@ class Model:
         Costruisce il grafo (self.G) inserendo tutti gli Hub (i nodi) presenti e filtrando le Tratte con
         guadagno medio per spedizione >= threshold (euro)
         """
-        # TODO
         hubs = DAO.readHub()
         hubs_dict = {hub.id: hub for hub in hubs}
         spedizioni = DAO.readSpedizioni(hubs_dict)
@@ -30,7 +29,6 @@ class Model:
         Restituisce il numero di Tratte (edges) del grafo
         :return: numero di edges del grafo
         """
-        # TODO
         return self.G.number_of_edges()
 
     def get_num_nodes(self):
@@ -38,7 +36,6 @@ class Model:
         Restituisce il numero di Hub (nodi) del grafo
         :return: numero di nodi del grafo
         """
-        # TODO
         return self.G.number_of_nodes()
 
     def get_all_edges(self):
@@ -46,6 +43,5 @@ class Model:
         Restituisce tutte le Tratte (gli edges) con i corrispondenti pesi
         :return: gli edges del grafo con gli attributi (il weight)
         """
-        # TODO
         return [(u, v, d['weight']) for u, v, d in self.G.edges(data=True)]
 
